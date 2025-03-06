f04bc035592f2926fc086c5392aeef691ea4d528
volleyballDeveloper95 <vladimirpanev95@gmail.com>
2025-03-06 09:41:01 +0200

    change

--- src/utils/Ownable.sol
+++ src/utils/Ownable.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.19;
 
 abstract contract Ownable {
-
     event OwnershipTransferred(address indexed user, address indexed newOwner);
 
     address public owner;
@@ -11,11 +10,13 @@ abstract contract Ownable {
         require(msg.sender == owner, "UNAUTHORIZED");
         _;
     }
+
     constructor(address _owner) {
         owner = _owner;
         emit OwnershipTransferred(address(0), _owner);
     }
 
+    //@audit - check of the new owner is a valid address
     function transferOwnership(address _owner) public virtual onlyOwner {
         owner = _owner;
         emit OwnershipTransferred(msg.sender, _owner);
